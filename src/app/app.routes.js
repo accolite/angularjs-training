adc9a0b7debeca15da30bf00edcb39c1d1af97bf
BarkhaAccolite <lakshmi.narayanan@accolite.com>
2016-07-29 21:32:30 +0530

    final

--- src/app/app.routes.js
+++ src/app/app.routes.js
@@ -11,42 +11,46 @@
 
         $urlRouterProvider.otherwise('/books/list');
 
-    	$stateProvider
-        .state('books', {
-            abstract: true,
-    		url: '/books',
-            template : "<div ui-view></div>",
-            resolve:{
-                bookList: ["Books", function(Books){
-                    return Books.loadAllBooks();
-                }],
-                inventoryDetail: ["Inventory", function(Inventory){
-                    return Inventory.loadInventory();   
-                }]
-            }
-    	})
-        .state('books.list', {
-            url: '/list',
-            templateUrl: 'src/app/books/books.html',
-            controller: 'booksCtrl'
-        })
-        .state('books.detail', {
-            url: '/detail/:id',
-            templateUrl: 'src/app/books/book-detail.html',
-            controller: 'bookDetailCtrl'
-        })
-        .state('users', {
+        $stateProvider
+            .state('books', {
+                abstract: true,
+                url: '/books',
+                template: "<div ui-view></div>",
+                resolve: {
+                    bookList: ["Books", function (Books) {
+                        return Books.loadAllBooks();
+                    }],
+                    inventoryDetail: ["Inventory", function (Inventory) {
+                        return Inventory.loadInventory();
+                    }]
+                }
+            })
+            .state('books.list', {
+                url: '/list',
+                templateUrl: 'src/app/books/books.html',
+                controller: 'booksCtrl'
+            })
+            .state('books.detail', {
+                url: '/detail/:id',
+                templateUrl: 'src/app/books/book-detail.html',
+                controller: 'bookDetailCtrl'
+            })
+            .state('users', {
                 url: '/users',
                 templateUrl: 'src/app/users/users.html',
                 controller: 'usersController',
                 controllerAs: 'users',
                 resolve: {
                     userListResponse: ["userService", function (userService) {
-                         var user = userService.fetchUsers();
-                         return user;
-                    }]
+                        var user = userService.fetchUsers();
+                        return user;
+                    }],
+                    bookList: ["Books", function (Books) {
+                        return Books.loadAllBooks();
+                    }],
+
                 }
-        });
+            });
     }
 
 })();
