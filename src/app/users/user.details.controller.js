6b3fb6b101c9fe873deb9b0355734c23f994ef3a
BarkhaAccolite <lakshmi.narayanan@accolite.com>
2016-07-29 21:21:33 +0530

    modal

--- src/app/users/user.details.controller.js
+++ src/app/users/user.details.controller.js
@@ -8,16 +8,17 @@
     userDetailsController.$inject = ["$uibModalInstance", "booksBorrowed", "Books"];
 
     function userDetailsController($uibModalInstance, booksBorrowed, Books) {
-
+        this.booksBorrowed = [];
         this.getBookDetails = function () {
             booksBorrowed.forEach(function (book) {
                 this.booksBorrowed.push(Books.getBookById(book.bookId));
             }.bind(this));
         }
-        console.log(booksBorrowed);
 
         this.close = function () {
             $uibModalInstance.close();
         }
+
+        this.getBookDetails();
     }
 })();
